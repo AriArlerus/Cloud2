a6180e13b547153d4b566e2fe49860d33e8867ff
ARI ARLERUS <108457826+AriArlerus@users.noreply.github.com>
2025-08-26 21:16:21 +0700

    test

--- public/js/product.js
+++ public/js/product.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.shownow').forEach(button => {
+    document.querySelectorAll('.product').forEach(button => {
         button.addEventListener('click', (event) => {
             window.location.href ='/navigation/product-list';
         });
